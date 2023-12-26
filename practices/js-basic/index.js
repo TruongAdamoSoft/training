f784ee36995197d008d9e9a9bb3f82d7dd560a9a
TerryTrinh <truong@menucoster.com>
2023-12-26 10:00:33 +0700

    js basic

---
+++ practices/js-basic/index.js
@@ -0,0 +1,26 @@
+/*
+Bài 1: Viết hàm tính tổng các phần tử trong mảng (param: array)
+ex: [1,2,3,4,5] => output: 15
+*/
+// TODO: Implement code here
+
+/*
+Bài 2: Cho một mảng object 
+object = [{name: "John", age: 26}, {name: "Pete", age: 30}, {name: Mary, age: 28}]
+
+viết hàm trả về một mảng tên và tuổi trung bình (param: object)
+output: {names: ["John", "Pete", "Mary"], ageAvg: 42}
+*/
+// TODO: Implement code here
+
+/*
+Bài 3: Viết hàm tính tổng số chẵn số lẻ từ một số n (param: n)
+ex: n = 5 => output: {even: 6, odd: 9}
+*/
+// TODO: Implement code here
+
+/*
+Bài 4: Viết hàm sắp xếp mảng theo thứ tự tăng dần hoặc giảm dần (param là một mảng và type = 'DESC' hoặc 'ASC)
+ex: arr = [1,4,2,5,3], type = "DESC" => output: [1,2,3,4,5]
+*/
+// TODO: Implement code here
