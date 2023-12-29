518f790787e9e7da4d2fcd89278cf6c89224d918
Duong Khai <khai.dq@adamo.software>
2023-12-29 17:47:39 +0700

    closure, bind, call, apply, Array methods

--- practices/js-advantive/array/index.js
+++ practices/js-advantive/array/index.js
@@ -5,6 +5,10 @@ Không sửa đổi mảng. Trả về mảng mới.
 */
 // TODO: Implement code here
 
+function filterRange(arr, a, b) {
+  return arr.filter(x => x > a && x < b);
+}
+
 /*
 Bài 2: Viết một hàm filterRangeInPlace(arr, a, b) lấy một mảng arr và loại bỏ khỏi nó tất cả các giá trị ngoại trừ những giá trị nằm giữa a và b.
 Nghiệm là: a ≤ arr [i] ≤ b.
@@ -13,6 +17,15 @@ filterRangeInPlace(arr, 1,3)
 console.log(arr) => [1,3]
 */
 // TODO: Implement code here
+function filterRangeInPlace(arr, a, b) {
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    if (element < a || element > b) {
+      arr.splice(i, 1);
+      i--;
+    }
+  }
+}
 
 /*
 Bài 3: Bạn có một mảng các đối tượng người dùng, mỗi đối tượng có name, surname và id. 
@@ -23,11 +36,24 @@ Sử dụng các method của array
 */
 // TODO: Implement code here
 
+function bai3(arr) {
+  return arr.map(obj => ({
+    id: obj.id,
+    fullName: obj.name + obj.surname
+  }));
+}
+
+
 /*
 Bài 4: Viết hàm getAverageAge(users) lấy một mảng đối tượng có thuộc tính tuổi và trả về tuổi trung bình.
 có sử dụng method reduce của array
 */
 // TODO: Implement code here
+function getAverageAge(users) {
+  return users.reduce((total, user) => {
+    total += user.age
+  }, 0) / users.length;
+}
 
 /*
 Bài 5: Cho một mảng n số nguyên. Đếm xem trong mảng mỗi số xuất hiện bao nhiêu lần trong mảng. 
@@ -52,6 +78,23 @@ Các số trong mảng có độ lớn từ 0 đến 1000. n <= 100. Yêu cầu:
 ] 
 */
 
+function print(nums) {
+  const map = new Map();
+  for (const x of nums) {
+    if (map.has(x)) {
+      value = map.get(x);
+      map.set(x, value + 1);
+    } else {
+      map.set(x, 1);
+    }
+  }
+  return map;
+}
+
+// convert map to output here
+
+
+
 /*
 bài 6: Cho mảng sau 
 
@@ -66,6 +109,11 @@ bài 6: Cho mảng sau
 Lọc ra các giá trị id là số dương trong mỗi object. 
 */
 // TODO: Implement code here
+
+function filterObj(arr) {
+  return arr.filter(obj => typeof(obj.id) == 'number' && obj.id > 0);
+}
+
 
 /*
 Bài 7: Viết chương trình để tính cước điện thoại bàn cho một hộ gia đình với các thông số như sau: 
